4652b175f203e9ef086c0334ebc3a510a41c92b6
이혜원 <starsrebirthyou@gmail.com>
2026-07-20 15:31:50 +0900

    feat: 채팅 기록 영속화, 알림, 타임라인, 조연 캐릭터 이벤트, 타이핑 이펙트, PWA 추가

---
+++ public/sw.js
@@ -0,0 +1,30 @@
+const CACHE = 'ansou-v1';
+const SHELL = ['/', '/index.html', '/manifest.json', '/icon.svg'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(SHELL)).catch(() => {}));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  // API 요청은 항상 네트워크로 (캐시하면 안 됨)
+  if (e.request.method !== 'GET' || e.request.url.includes('/api/')) return;
+  e.respondWith(
+    fetch(e.request)
+      .then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE).then((c) => c.put(e.request, copy)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
